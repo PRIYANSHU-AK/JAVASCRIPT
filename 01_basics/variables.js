7e1ce331479c19f3ffd7e2d8163cdef202e2eede
PRIYANSHU-AK <priyanshusharma1367@gmail.com>
2026-01-19 13:47:30 +0000

    configuger done

---
+++ 01_basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = "387942407"
+let accountEmail = "priyanshu@google.com"
+var accountPassword = "12@34"
+accountCity = "Delhi"
+let accountState;
+
+// accountId = "2323" not allowed const can't be changed
+
+accountEmail = "priyanshu@microsoft.com"
+accountPassword = "45@23"
+accountCity = "Mumbai"
+
+console.log(accountId);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
