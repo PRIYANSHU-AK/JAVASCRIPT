7288c2dd093398925b0c291d27eb2d265fba6980
PRIYANSHU-AK <priyanshusharma1367@gmail.com>
2026-01-20 12:56:00 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,27 @@
+// "use strict";   treat all JS code as a newer version
+
+// alert(3 + 3 ) we are using nodejs, not browser 
+
+console.log(3
+    +
+     3)   // code readability should be high 
+
+console.log("priyanshu") ;
+
+let name = "priyanshu"
+let age = 16
+let isLoggedIn = false 
+
+// number => 2 to power 53
+//  bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+//  undefined =>
+// symbol => unique
+
+console.log(typeof undefined);  //  undefined 
+console.log(typeof null);  // object
+
+
+    
