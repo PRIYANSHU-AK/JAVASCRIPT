279983a48ed0ea3f3fc0f66754d87b067d77fac1
PRIYANSHU-AK <priyanshusharma1367@gmail.com>
2026-01-26 10:11:36 +0000

    nums and math

---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,38 @@
+const score = 100
+// console.log(score);
+
+const balance = new Number(230)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+const otherNumber = 125.789
+
+// console.log(otherNumber.toFixed(2));
+// console.log(otherNumber.toPrecision(3));
+
+const toNumber = 10000000
+// console.log(toNumber.toLocaleString());
+
+// *****************************  MATHS **********************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(-4));
+// console.log(Math.ceil(4.7));
+// console.log(Math.floor(4.6));
+// console.log(Math.max(4 ,6 , 8 ,3 ));
+// console.log(Math.min(4 ,6 , 8 ,3 ));
+
+// console.log(Math.random())
+// console.log(Math.random()*10 + 1)
+// console.log((Math.random()*10)  + 1)
+// console.log(Math.floor(Math.random()*10)  + 1)
+
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1 ))+ min);
+
+
---
+++ 01_basics/string.js
@@ -0,0 +1,33 @@
+const name = "Priyanshu"
+const RepoCount = 20
+
+// console.log(name + RepoCount + " duniya ");
+console.log(`Hello my name is ${name} and my repo Count is ${RepoCount}`);
+
+const gameName = new String('Priyanshupc')
+
+// console.log(gameName[2]);
+console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase);
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('h'));
+
+const newString = gameName.substring(0 , 5);
+// console.log(newString);
+
+const anotherstring = gameName.slice(-9 ,5);
+// console.log(anotherstring);
+
+const newStringone = "    Priyanshu   ";
+// console.log(newStringone);
+// console.log(newStringone.trim());
+
+const Url = "https://priyanshu.com/priyanshu%50sharma"
+// console.log(Url.replace('%50' , '-'));
+
+//  console.log(Url.includes('beauty'));
+ 
+//  console.log(gameName.split('u'));
+ 
